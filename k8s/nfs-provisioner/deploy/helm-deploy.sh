aaf01e914bbf516e7574c9bd7c7c516e052f14b0
John Chever <cheverjonathan@gmail.com>
2023-05-03 10:14:50 +0800

    chore: k8s nfs-provisioner (#12)

---
+++ k8s/nfs-provisioner/deploy/helm-deploy.sh
@@ -0,0 +1,4 @@
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --set nfs.server=10.100.201.184 \
+    --set nfs.path=/k8s/storage
