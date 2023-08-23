2e3047d730dbb137a329f3b75b29c3434804dbc9
Chever John <cheverjonathan@gmail.com>
2023-08-23 12:55:38 +0800

    chore: remove some cfgs

--- nvim/lua/lsp/setup.lua
+++ nvim/lua/lsp/setup.lua
@@ -32,18 +32,7 @@ require("mason").setup({
 require("mason-lspconfig").setup({
   -- 确保安装，根据需要填写
   ensure_installed = {
-    "sumneko_lua",
-    "tsserver",
-    "tailwindcss",
     "bashls",
-    "cssls",
-    "dockerls",
-    "emmet_ls",
-    "html",
-    "jsonls",
-    "pyright",
-    "rust_analyzer",
-    "taplo",
     "yamlls",
     "gopls",
   },
