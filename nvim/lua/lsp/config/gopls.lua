8ca47bfd54aefa113753df12db577c77a8c778ad
Chever John <cheverjonathan@gmail.com>
2023-08-23 15:27:36 +0800

    feat(gopls): perf the golang experience

--- nvim/lua/lsp/config/gopls.lua
+++ nvim/lua/lsp/config/gopls.lua
@@ -22,10 +22,10 @@ local opts = {
         end
 
         -- 绑定快捷键
-        -- require('keybindings').mapLSP(buf_set_keymap)
+        require('keybindings').mapLSP(buf_set_keymap)
 
         -- 保存时自动格式化
-        -- vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
+        vim.cmd('autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()')
     end,
 }
 
