16fdee0bf04dfc3901c9d0ea41039c1ed53dd21f
Chever John <cheverjonathan@gmail.com>
2024-02-08 00:30:24 +0800

    chore(ibl): upgrade indent_blankline from v2 to v3 (#30)

--- nvim/lua/plugin-config/indent-blankline.lua
+++ nvim/lua/plugin-config/indent-blankline.lua
@@ -1,15 +1,18 @@
-local status, ident_blankline = pcall(require, 'indent_blankline')
+-- 首先是使用 pcall 函数来尝试加载 indent_blankline 插件，如果加载失败，就会返回一个错误信息，
+-- 然后通过 vim.notify 函数来提示用户没有找到 indent_blankline 插件。
+local status, ident_blankline = pcall(require('ibl'), "indent_blankline")
 if not status then
     vim.notify('没有找到 indent_blankline')
     return
 end
 
 ident_blankline.setup({
-    -- 空行占位
+    -- 空行占位，用于设置空行的占位字符
     space_char_blankline = ' ',
     -- 用 treesitter 判断上下文
     show_current_context = true,
     show_current_context_start = true,
+    -- 用于设置上下文的模式，例如类、函数、方法等
     context_patterns = {
         'class',
         'function',
