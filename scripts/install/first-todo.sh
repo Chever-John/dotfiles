6af680730ff4544b7f3a6835de7e9e1e482a0a97
Chever John <cheverjonathan@gmail.com>
2024-07-10 18:08:52 +0800

    chore: first to do

---
+++ scripts/install/first-todo.sh
@@ -0,0 +1,8 @@
+sudo apt-get install git
+
+git config --global user.name "Chever John"
+
+git config --global user.email cheverjonathan@gmail.com
+
+ssh-keygen -t rsa -C "cheverjonathan@gmail.com"
+
