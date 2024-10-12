3e2e74c982a459bd1f72c3ef559503249d6a007a
Chever John <cheverjonathan@gmail.com>
2024-10-12 11:30:24 +0800

    fix: checkhealth problem

--- nvim/lua/plugin-config/nvim-treesitter.lua
+++ nvim/lua/plugin-config/nvim-treesitter.lua
@@ -5,20 +5,32 @@ if not status then
 end
 
 treesitter.setup({
-    -- 安装 language parser
-    -- :TSInstallInfo 命令查看支持的语言
-    ensure_installed = {
-       -- 'yaml',
-        'go',
-       -- 'bash',
-        'lua',
-       -- 'markdown',
-    },
-    -- 启用代码高亮模块
-    highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-    },
+  -- 安装 language parser
+  -- :TSInstallInfo 命令查看支持的语言
+  ensure_installed = {
+    -- 'project',
+    'gitignore',
+    -- 'go',
+    'go',
+    'gomod',
+    'gosum',
+    -- 'bash',
+    'lua',
+    -- 'json',
+    'json',
+    -- 'query',
+    'query',
+    -- 'vim',
+    'vim',
+    'vimdoc',
+    'markdown',
+    'markdown_inline',
+  },
+  -- 启用代码高亮模块
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
 
     -- 启用增量选择模块
     incremental_selection = {
