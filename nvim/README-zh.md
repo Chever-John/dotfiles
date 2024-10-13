2828fad3c9735a974eedbfbf35539497e5adee4d
Chever John <cheverjonathan@gmail.com>
2024-10-13 15:38:34 +0800

    feat: perf the lua language server (#56)
    * chore: rebase main
    
    * chore: add sth
    
    * feat: no finish
    
    * feat: add sth
    
    * doc: update doc
    
    * feat: try to change from sumeneko_lua to lua_ls

--- nvim/README-zh.md
+++ nvim/README-zh.md
@@ -1,5 +1,7 @@
 # NeoVim
 
+[English](README.md)
+
 我更新了最全套的
 
 ## Neovim（Lua 版本）
@@ -89,11 +91,15 @@ https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/FiraCode/Regul
 总而言之，不管你是什么系统，macOS 还是 ubuntu，最后一切的最开始一定是要有 neovim 的。
 
 ```bash
-➜  ~ nvim --version
-NVIM v0.7.0
+╰─[:(] % nvim --version
+NVIM v0.9.5
 Build type: Release
-LuaJIT 2.1.0-beta3
-Compiled by brew@HMBRW-A-001-M1-004.local
+LuaJIT 2.1.1710088188
+
+   system vimrc file: "$VIM/sysinit.vim"
+  fall-back for $VIM: "/opt/homebrew/Cellar/neovim/0.9.5/share/nvim"
+
+Run :checkhealth for more info
 ```
 
 ##### 拉取项目
@@ -186,7 +192,9 @@ GitHub: <https://github.com/ctrlpvim/ctrlp.vim>
 
 这是一个注释用的插件。具体操作流程如下 gif 图所示：
 
-![shenmegui](https://cdn.mr8god.cn/img/Kapture2022-08-07123208.gif)
+```shell
+链接已失效
+```
 
 ### vim-gitgutter
 
@@ -207,8 +215,6 @@ ln -s /nvim/init.vim ~/.config/nvim/init.vim
 ### 如何更新 Neovim
 
 我想，当我某一天使用 nvim 打开某个文件夹，这丫的，居然给我爆出这个错误，错误如下图所示：
-
-![错误报错图](https://cdn.mr8god.cn/img/error_image_2022-08-07_10-44-38.png)
 
 我就知道我摊上事了，首先汇报我的系统版本：
 
@@ -335,9 +341,34 @@ Highlights broken after update with query: invalid structure #3092
 
 [leetcode.nvim](docs/plugins/leetcode-nvim-zh.md)：用于 leetcode 刷题的一个插件。
 
+我的整体美化思路，文档参见[这里](docs/plugins/beautify.md)
+
 ## 乱码问题
 
 我在使用 vscode 打开这个项目的时候，很多都是乱码图标，其实只需要你上 https://www.nerdfonts.com/font-downloads 这个网站，
 然后下载一个适配的字体即可，目前我常用的字体为Nerdfont，经过测试这款字体都可以。
 
+## Changelog
 
+### 2024 年 10 月 13 日
+
+我发现原本的 lua 插件失效了，sumneko_lua 将 deprecated，我们需要使用最新的 lua_ls。
+
+![image-20241013153129325](assets/image-20241013153129325.png)
+
+所以我今天主要是将这个插件更新。
+
+我所做的操作也只是如下：
+
+```lua
+local servers = {
+  -- sumneko_lua = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
+  lua_ls = require("lsp.config.lua"),
+}
+```
+
+然后可以发现，我原先的一些自动提示啦，一些好用的功能都用不了了。这肯定是因为 lua_ls 和 sumneko_lua 之间的配置问题。我想我需要进行一些研究
+
+## TODO
+
+- [ ] 修复 lua lsp 的问题。lua 的 ls 从 sumneko_lua 到 lua_ls，这中间有一些问题亟待解决。
--- nvim/lua/lsp/setup.lua
+++ nvim/lua/lsp/setup.lua
@@ -1,81 +1,33 @@
-local status, mason = pcall(require, "mason")
-if not status then
-  vim.notify("没有找到 mason")
-  return
-end
-
-local status, mason_config = pcall(require, "mason-lspconfig")
-if not status then
-  vim.notify("没有找到 mason-lspconfig")
-  return
-end
-
-local status, lspconfig = pcall(require, "lspconfig")
-if not status then
-  vim.notify("没有找到 lspconfig")
-  return
-end
-
--- :h mason-default-settings
-require("mason").setup({
-  ui = {
-    icons = {
-      package_installed = "✓",
-      package_pending = "➜",
-      package_uninstalled = "✗",
-    },
-  },
-})
-
--- mason-lspconfig uses the `lspconfig` server names in the APIs it exposes - not `mason.nvim` package names
--- https://github.com/williamboman/mason-lspconfig.nvim/blob/main/doc/server-mapping.md
-require("mason-lspconfig").setup({
-  -- 确保安装，根据需要填写
-  ensure_installed = {
-    "bashls",
-    "yamlls",
-    "gopls",
-  },
-})
-
-
+local lsp_installer = require("nvim-lsp-installer")
 
 -- 安装列表
 -- { key: 语言 value: 配置文件 }
 -- key 必须为下列网址列出的名称
 -- https://github.com/williamboman/nvim-lsp-installer#available-lsps
 local servers = {
-    -- sumneko_lua = require('lsp.config.lua'), -- lua/lsp/config/lua.lua
-    bashls = require('lsp.config.bash'),
-
-    -- frontend
-    -- html = require('lsp.config.html'),
-    -- cssls = require('lsp.config.css'),
-    -- emmet_ls = require('lsp.config.emmet'),
-    -- jsonls = require('lsp.config.json'),
-    -- tsserver = require('lsp.config.ts'),
-
-    -- go
-    gopls = require('lsp.config.gopls'),
-
-    -- docker
-    -- dockerls = require("lsp.config.docker"),
-
-    -- dart
-    -- dartls = require('lsp.config.dart'),
-
-    -- rust
-    -- rust_analyzer = require('lsp.config.rust'),
+  -- sumneko_lua = require("lsp.config.lua"), -- lua/lsp/config/lua.lua
+  lua_ls = require("lsp.config.lua"),
 }
-
-for name, config in pairs(servers) do
-  if config ~= nil and type(config) == "table" then
-    -- 自定义初始化配置文件必须实现on_setup 方法
-    config.on_setup(lspconfig[name])
-  else
-    -- 使用默认参数
-    lspconfig[name].setup({})
+-- 自动安装 Language Servers
+for name, _ in pairs(servers) do
+  local server_is_found, server = lsp_installer.get_server(name)
+  if server_is_found then
+    print(name .. "has beed installed")
+    if not server:is_installed() then
+      print("Installing " .. name)
+      server:install()
+    end
   end
 end
 
-require("lsp.ui")
+lsp_installer.on_server_ready(function(server)
+    local config = servers[server.name]
+    if config == nil then
+        return
+    end
+    if config.on_setup then
+        config.on_setup(server)
+    else
+        server:setup({})
+    end
+end)
