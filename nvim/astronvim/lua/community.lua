c16d5a9ede51a1494d861f45ae87e50a42849b95
Chever John <cheverjonathan@gmail.com>
2024-10-14 21:19:39 +0800

    feat: add my astronvim configurations

---
+++ nvim/astronvim/lua/community.lua
@@ -0,0 +1,13 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.python" },
+  -- import/override with your plugins folder
+}
