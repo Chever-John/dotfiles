78488f075a43efda0f567443ad1c17eedd93ed0a
Chever John <cheverjonathan@gmail.com>
2024-10-22 22:09:32 +0800

    feat: i want a transport vim!!! (#63)
    * feat: setup first
    
    * feat: status before change the astroui.lua
    
    * feat: setup successfully
    
    * chore: update .gitignore
    
    * chore: update CustomVim
    
    * feat: add change nvim script
    
    * chore: update gitignore
    
    * chore: change sth
    
    * feat: add sh

--- nvim/astronvim/lua/plugins/astroui.lua
+++ nvim/astronvim/lua/plugins/astroui.lua
@@ -1,5 +1,3 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroUI provides the basis for configuring the AstroNvim User Interface
 -- Configuration documentation can be found with `:h astroui`
 -- NOTE: We highly recommend setting up the Lua Language Server (`:LspInstall lua_ls`)
@@ -13,14 +11,52 @@ return {
     -- change colorscheme
     colorscheme = "astrodark",
     -- AstroUI allows you to easily modify highlight groups easily for any and all colorschemes
-    highlights = {
-      init = { -- this table overrides highlights in all themes
-        -- Normal = { bg = "#000000" },
-      },
-      astrodark = { -- a table of overrides/changes when applying the astrotheme theme
-        -- Normal = { bg = "#000000" },
-      },
-    },
+    -- highlights = {
+    --   init = { -- this table overrides highlights in all themes
+    --     -- Normal = { bg = "#000000" },
+    --   },
+    --   astrodark = { -- a table of overrides/changes when applying the astrotheme theme
+    --     -- Normal = { bg = "#000000" },
+    --   },
+    -- },
+
+    -- transport config in highlights
+    highlights = (function()
+      local transparent_bg = { bg = "NONE", ctermbg = "NONE" }
+
+      -- Common highlight groups with transparent background
+      local common_highlights = {
+        Normal = transparent_bg,
+        NormalNC = transparent_bg,
+        SignColumn = transparent_bg,
+        LineNr = transparent_bg,
+        Folded = transparent_bg,
+        NonText = transparent_bg,
+        SpecialKey = transparent_bg,
+        VertSplit = transparent_bg,
+        EndOfBuffer = transparent_bg,
+
+        -- Status and Tab lines
+        StatusLine = transparent_bg,
+        -- StatusLineNC = { bg = "NONE", ctermbg = "NONE" },
+        StatusLineNC = { bg = "#1c1c1c", ctermbg = "NONE" },
+        TabLine = transparent_bg,
+        TabLineFill = transparent_bg,
+        TabLineSel = transparent_bg,
+
+        -- Config for neo-tree
+        NeoTreeNormal = transparent_bg,
+        NeoTreeNormalNC = transparent_bg,
+        NeoTreeEndOfBuffer = transparent_bg,
+        NeoTreeVertSplit = transparent_bg,
+      }
+
+      return {
+        init = common_highlights, -- Overrides highlights in all themes
+        astrodark = common_highlights, -- Overrides for the astrodark theme
+      }
+    end)(),
+
     -- Icons can be configured throughout the interface
     icons = {
       -- configure the loading of the lsp in the status line
