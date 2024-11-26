53b14ae1b65820c1a0e3c79122f863f3d14b1c18
Chever John <cheverjonathan@gmail.com>
2024-11-26 17:12:33 +0800

    feat: finish the all go config

--- nvim/astronvim/lua/plugins/pack-go.lua
+++ nvim/astronvim/lua/plugins/pack-go.lua
@@ -72,7 +72,7 @@ return {
   {
     "nvim-neotest/neotest",
     optional = true,
-    dependencies = { "nvim-neotest/neotest-go", config = function() end },
+    dependencies = { "nvim-neotest/neotest-go" },
     opts = function(_, opts)
       if not opts.adapters then opts.adapters = {} end
       table.insert(opts.adapters, require "neotest-go"(require("astrocore").plugin_opts "neotest-go"))
