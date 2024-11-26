b0d2aff0b6d37615e1ceb7b6253711068e890116
Chever John <cheverjonathan@gmail.com>
2024-11-26 22:07:00 +0800

    feat: change golang format

--- nvim/astronvim/lua/plugins/pack-go.lua
+++ nvim/astronvim/lua/plugins/pack-go.lua
@@ -1,4 +1,140 @@
+--TODO: https://github.com/golang/go/issues/60903
+local set_mappings = require("astrocore").set_mappings
+
+local function preview_stack_trace()
+  local current_line = vim.api.nvim_get_current_line()
+  local patterns_list = {
+    "([^%s]+/[^%s]+%.go):(%d+)", -- 匹配文件路径和行号
+  }
+
+  local function try_patterns(patterns, line)
+    for _, pattern in ipairs(patterns) do
+      local filepath, line_nr = string.match(line, pattern)
+      if filepath and line_nr then return filepath, tonumber(line_nr), 0 end
+    end
+    return nil, nil, nil
+  end
+
+  local filepath, line_nr, column_nr = try_patterns(patterns_list, current_line)
+  if filepath then
+    vim.cmd ":wincmd k"
+    vim.cmd("e " .. filepath)
+    vim.api.nvim_win_set_cursor(0, { line_nr, column_nr })
+  end
+end
+
+-- NOTE: gopls commands
+-- GoTagAdd add tags
+-- GOTagRm remove tags
+-- GoCmt add cmt
+-- GoFillStruct	auto fill struct
+-- GoFillSwitch	fill switch
+-- GoIfErr	Add if err
+-- GoFixPlurals	change func foo(b int, a int, r int) -> func foo(b, a, r int)
+
+---@type LazySpec
 return {
+  {
+    "AstroNvim/astrocore",
+    ---@type AstroCoreOpts
+    opts = { filetypes = { extension = { api = "goctl" } } },
+  },
+  {
+    "AstroNvim/astrolsp",
+    optional = true,
+    ---@type AstroLSPOpts
+    opts = {
+      ---@diagnostic disable: missing-fields
+      config = {
+        gopls = {
+          on_attach = function(client, _)
+            vim.api.nvim_create_autocmd({ "TermOpen", "TermClose", "BufEnter" }, {
+              pattern = "*",
+              desc = "Jump to error line",
+              callback = function()
+                local buf_name = vim.api.nvim_buf_get_name(0)
+                if vim.bo.filetype == "dap-repl" and buf_name:match "%[dap%-repl%-%d+%]" then
+                  set_mappings({
+                    n = {
+                      ["gd"] = {
+                        preview_stack_trace,
+                        desc = "Jump to error line",
+                      },
+                    },
+                  }, { buffer = true })
+                end
+              end,
+            })
+
+            set_mappings({
+              n = {
+                ["<Leader>fi"] = {
+                  [[<cmd>lua require'telescope'.extensions.goimpl.goimpl{}<CR>]],
+                  desc = "Find Go interface implementation",
+                },
+              },
+            }, { buffer = true })
+
+            if not client.server_capabilities.semanticTokensProvider then
+              local semantic = client.config.capabilities.textDocument.semanticTokens
+              client.server_capabilities.semanticTokensProvider = {
+                full = true,
+                legend = {
+                  tokenTypes = semantic.tokenTypes,
+                  tokenModifiers = semantic.tokenModifiers,
+                },
+                range = true,
+              }
+            end
+          end,
+          settings = {
+            gopls = {
+              analyses = {
+                ST1003 = false,
+                SA5008 = false,
+                fieldalignment = false,
+                fillreturns = true,
+                nilness = true,
+                nonewvars = true,
+                shadow = true,
+                undeclaredname = true,
+                unreachable = true,
+                unusedparams = true,
+                unusedwrite = true,
+                useany = true,
+              },
+              codelenses = {
+                gc_details = false, -- Show a code lens toggling the display of gc's choices.
+                generate = true, -- show the `go generate` lens.
+                regenerate_cgo = true,
+                test = true,
+                tidy = true,
+                upgrade_dependency = true,
+                vendor = true,
+              },
+              hints = {
+                assignVariableTypes = true,
+                compositeLiteralFields = true,
+                compositeLiteralTypes = true,
+                constantValues = true,
+                functionTypeParameters = true,
+                parameterNames = true,
+                rangeVariableTypes = true,
+              },
+              completeUnimported = true,
+              diagnosticsDelay = "500ms",
+              gofumpt = true,
+              matcher = "Fuzzy",
+              semanticTokens = true,
+              staticcheck = true,
+              symbolMatcher = "fuzzy",
+              usePlaceholders = false,
+            },
+          },
+        },
+      },
+    },
+  },
   -- Golang support
   {
     "nvim-treesitter/nvim-treesitter",
@@ -8,24 +144,6 @@ return {
         opts.ensure_installed =
           require("astrocore").list_insert_unique(opts.ensure_installed, { "go", "gomod", "gosum", "gowork" })
       end
-    end,
-  },
-
-  {
-    "jay-babu/mason-null-ls.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(
-        opts.ensure_installed,
-        { "gomodifytags", "iferr", "impl", "gotests", "goimports" }
-      )
-    end,
-  },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "gopls" })
     end,
   },
   {
@@ -39,19 +157,25 @@ return {
     end,
   },
   {
-    "leoluz/nvim-dap-go",
-    ft = "go",
+    "mfussenegger/nvim-dap",
+    optional = true,
     dependencies = {
-      "mfussenegger/nvim-dap",
       {
-        "jay-babu/mason-nvim-dap.nvim",
-        optional = true,
-        opts = function(_, opts)
-          opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, { "delve" })
-        end,
+        "leoluz/nvim-dap-go",
+        opts = {},
       },
     },
-    opts = {},
+  },
+  {
+    "edolphin-ydf/goimpl.nvim",
+    ft = "go",
+    requires = {
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-lua/popup.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+      { "nvim-treesitter/nvim-treesitter" },
+    },
+    config = function() require("telescope").load_extension "goimpl" end,
   },
   {
     "olexsmir/gopher.nvim",
@@ -69,15 +193,36 @@ return {
     },
     opts = {},
   },
-  -- {
-  --   "nvim-neotest/neotest",
-  --   optional = true,
-  --   dependencies = { "nvim-neotest/neotest-go" },
-  --   opts = function(_, opts)
-  --     if not opts.adapters then opts.adapters = {} end
-  --     table.insert(opts.adapters, require "neotest-go"(require("astrocore").plugin_opts "neotest-go"))
-  --   end,
-  -- },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = { "nvim-neotest/neotest-go" },
+    opts = function(_, opts)
+      if not opts.adapters then opts.adapters = {} end
+      table.insert(opts.adapters, require "neotest-go"(require("astrocore").plugin_opts "neotest-go"))
+    end,
+  },
+  {
+    "chaozwn/goctl.nvim",
+    ft = "goctl",
+    enabled = vim.fn.executable "goctl" == 1,
+    opts = function()
+      local group = vim.api.nvim_create_augroup("GoctlAutocmd", { clear = true })
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        pattern = "goctl",
+        callback = function()
+          -- set up format keymap
+          vim.keymap.set(
+            "n",
+            "<Leader>lf",
+            "<Cmd>GoctlApiFormat<CR>",
+            { silent = true, noremap = true, buffer = true, desc = "Format Buffer" }
+          )
+        end,
+      })
+    end,
+  },
   {
     "stevearc/conform.nvim",
     optional = true,
