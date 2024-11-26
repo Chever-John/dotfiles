36ab53f02453b9dbb42d30c7225f6e8e9f6855e1
Chever John <cheverjonathan@gmail.com>
2024-11-26 22:12:05 +0800

    chore: update golang config

--- nvim/astronvim/lua/plugins/astrolsp.lua
+++ nvim/astronvim/lua/plugins/astrolsp.lua
@@ -37,42 +37,12 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      "gopls",
       -- "pyright"
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
     config = {
-      gopls = {
-        settings = {
-          gopls = {
-            analyses = {
-              ST1003 = true,
-              fieldalignment = false,
-              fillreturns = true,
-              nilness = true,
-              nonewvars = true,
-              shadow = true,
-              undeclaredname = true,
-              unreachable = true,
-              unusedparams = true,
-              unusedwrite = true,
-              useany = true,
-            },
-            codelenses = {
-              gc_details = true, -- Show a code lens toggling the display of gc's choices.
-              generate = true, -- show the `go generate` lens.
-              regenerate_cgo = true,
-              test = true,
-              tidy = true,
-              upgrade_dependency = true,
-              vendor = true,
-            },
-            staticcheck = true,
-            gofumpt = true,
-          },
-        },
-      },
+      -- clangd = { capabilities = { offsetEncoding = "utf-8" } },
     },
     -- customize how language servers are attached
     handlers = {
