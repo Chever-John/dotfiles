946b7cff199971655784e7578f76a341c930662e
Chever John <cheverjonathan@gmail.com>
2024-11-28 11:51:02 +0800

    chore: add macism

--- nvim/astronvim/lua/plugins/im-select.lua
+++ nvim/astronvim/lua/plugins/im-select.lua
@@ -3,7 +3,7 @@ return {
   "chaozwn/im-select.nvim",
   lazy = false,
   opts = {
-    -- default_command = "macism",
+    default_command = "macism",
     default_main_select = "im.rime.inputmethod.Squirrel.Hans",
     set_previous_events = { "InsertEnter", "FocusLost" },
   },
