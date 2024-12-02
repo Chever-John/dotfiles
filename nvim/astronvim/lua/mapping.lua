e3a5c9f4a5c578fe51d19288991e8f54ae1312e6
Chever John <cheverjonathan@gmail.com>
2024-12-02 14:26:32 +0800

    chore: update nvim-lint

--- nvim/astronvim/lua/mapping.lua
+++ nvim/astronvim/lua/mapping.lua
@@ -63,7 +63,7 @@ function M.core_mappings(mappings)
     if vim.fn.executable "lazygit" == 1 then
       maps.n["<Leader>tl"] = {
         require("utils").toggle_lazy_git(),
-        desc = "ToggleTerm lazygit from Chever",
+        desc = "ToggleTerm lazygit",
       }
     end
 
